82fd13e1c0f163ae80b10f89db7015bbcbd5ce79
Ross Mehringer <rossmehringer42@gmail.com>
2024-04-04 18:13:18 -0400

    Pr12 3:
    :

--- src/test/java/edu/iu/remehrin/primesservice/service/PrimesServiceTest.java
+++ src/test/java/edu/iu/remehrin/primesservice/service/PrimesServiceTest.java
@@ -6,25 +6,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PrimesServiceTest {
     PrimesService primesService = new PrimesService();
-//    @Test
-//    void _45IsNotPrime(){
-//        int n = 45;
-//        boolean expected = false;
-//        boolean actual = primesService.isPrime(n);
-//        assertEquals(expected, actual);
-//    }
-//    @Test
-//    void _539828945930573IsNotPrime(){
-//        long n = 539828945930573L;
-//        boolean expected = false;
-//        boolean actual = primesService.isPrime(n);
-//        assertEquals(expected, actual);
-//    }
-//    @Test
-//    void _285191IsPrime(){
-//        long n = 285191;
-//        boolean expected = true;
-//        boolean actual = primesService.isPrime(n);
-//        assertEquals(expected, actual);
-//    }
+    @Test
+    void _45IsNotPrime(){
+        int n = 45;
+        boolean expected = false;
+        boolean actual = primesService.isPrime(n);
+        assertEquals(expected, actual);
+    }
+    @Test
+    void _539828945930573IsNotPrime(){
+        long n = 539828945930573L;
+        boolean expected = false;
+        boolean actual = primesService.isPrime(n);
+        assertEquals(expected, actual);
+    }
+    @Test
+    void _285191IsPrime(){
+        long n = 285191;
+        boolean expected = true;
+        boolean actual = primesService.isPrime(n);
+        assertEquals(expected, actual);
+    }
 }
